51879174fe49597972dc45ce307c8c9c94b5958f
yutaro <yutaro256256@gmail.com>
2019-11-01 13:28:25 +0000

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  
+  before_save { self.email.downcase! }
     
-    has_secure_password
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
     
 end
