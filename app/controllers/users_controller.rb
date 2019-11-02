a8e0191e17f11342ba1224db20d81760c9b7145d
yutaro <yutaro256256@gmail.com>
2019-11-02 10:55:17 +0000

    add users/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'ユーザーを登録しました'
+      flash[:success] = "ユーザーを登録しました#{@user.image}"
       redirect_to @user
     else
       flash[:danger] = 'ユーザーの登録に失敗しました'
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
