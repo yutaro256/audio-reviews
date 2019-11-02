e0fe3328f9408af7a93daf23dbf12db20cee0cdd
yutaro <yutaro256256@gmail.com>
2019-11-02 19:53:12 +0000

    add items_controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: 'toppages#top'
   
   resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+  resources :items
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
