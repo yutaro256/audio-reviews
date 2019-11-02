c710b10344bec327781f61fd871ed78f0d840637
yutaro <yutaro256256@gmail.com>
2019-11-02 20:24:48 +0000

    add validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
-    has_many :likes
-    has_many :interested, through: :likes, resource: :user
+  
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :maker, presence: true, length: { maximum: 30 }
+  validates :price, numericality: { only_integer: true }
+  
+# imageカラムにアップローダーの機能を付与
+  mount_uploader :image, ImageUploader
+  
+  has_many :likes
+  has_many :interested, through: :likes, resource: :user
 end
