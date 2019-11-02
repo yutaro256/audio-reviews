9ec848f12e26476800d59bdf694f4192bd4df31a
yutaro <yutaro256256@gmail.com>
2019-11-02 10:56:11 +0000

    add session_routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: 'toppages#top'
   
   resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
