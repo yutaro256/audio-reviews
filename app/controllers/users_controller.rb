72bea69822b1ba57c28a61d59927c13060566c53
yutaro <yutaro256256@gmail.com>
2019-11-03 12:41:15 +0000

    remove dead code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "ユーザーを登録しました#{@user.image}"
+      flash[:success] = "ユーザーを登録しました"
       redirect_to @user
     else
       flash[:danger] = 'ユーザーの登録に失敗しました'
