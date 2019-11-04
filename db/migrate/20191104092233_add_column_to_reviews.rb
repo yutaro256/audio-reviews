7c2ea524645c0d8ad1e13a43615134bd6359dc63
yutaro <yutaro256256@gmail.com>
2019-11-04 09:26:41 +0000

    add column

---
+++ db/migrate/20191104092233_add_column_to_reviews.rb
@@ -0,0 +1,6 @@
+class AddColumnToReviews < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :reviews, :user, foreign_key: true
+    add_reference :reviews, :item, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_084803) do
+ActiveRecord::Schema.define(version: 2019_11_04_092233) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name_item"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 2019_11_04_084803) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_reviews_on_item_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -51,4 +55,6 @@ ActiveRecord::Schema.define(version: 2019_11_04_084803) do
 
   add_foreign_key "likes", "items"
   add_foreign_key "likes", "users"
+  add_foreign_key "reviews", "items"
+  add_foreign_key "reviews", "users"
 end
