76524d07fb31d401a6f880ac427ae78478091575
yutaro <yutaro256256@gmail.com>
2019-11-06 16:27:57 +0000

    fix module ReviewsHelper

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,8 +1,8 @@
 module ReviewsHelper
 # 詳細表示したレビューがログインユーザーが投稿したものか判定  
   def authority?
-    if current_user.reviews.find_by(id: params[:id])
-      true
+    if current_user
+      !!current_user.reviews.find_by(id: params[:id])
     else
       false
     end
