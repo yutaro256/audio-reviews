78a52189a7c267b9738b503b88a0ed49bc7666a5
yutaro <yutaro256256@gmail.com>
2019-11-11 03:41:47 +0000

    fix redirect path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "ユーザーを登録しました"
-      redirect_to @user
+      redirect_to login_url
     else
       flash.now[:danger] = 'ユーザーの登録に失敗しました'
       render :new
